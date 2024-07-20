6af23f47c92621e8e8846c98ea27481ec7341b18
unnmd88 <unnmd88@gmail.com>
2024-07-20 12:24:32 +0300

    add conflicts

--- toolkit/urls.py
+++ toolkit/urls.py
@@ -18,9 +18,6 @@ urlpatterns = [
 
     path("calc_conflicts/", views.data_for_calc_conflicts, name='calc_conflicts'),
 
-
-
-
     # path('toolkit/', views.index, name='toolkit'),
     # path('tabs/<int:tabs_id>/', views.tabs, name='tabs_id'), # http://127.0.0.1:8000/tabs/1/
     # path('tabs/<slug:tabs_slug>/', views.tabs_by_slug, name='tabs_slug'),  # http://127.0.0.1:8000/tabs/1/
